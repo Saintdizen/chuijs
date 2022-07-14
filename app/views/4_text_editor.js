14305b8d86d588676f654ef904d3538f3bc21908
Чувахин Иван <saintdizen@gmail.com>
2022-07-14 20:06:29 +0300

    Редактор текста в5

--- app/views/4_text_editor.js
+++ app/views/4_text_editor.js
@@ -6,7 +6,7 @@ class TextEditorPage extends Page {
         this.setTitle('Редактор текста');
         this.setMain(true)
 
-        let textedit = new TextEditor("300px")
+        let textedit = new TextEditor(300)
         this.add(textedit)
     }
 }
--- framework/components/chui_text_editor.js
+++ framework/components/chui_text_editor.js
@@ -1,4 +1,6 @@
 const {Icon, Icons} = require('./chui_icons');
+const {Label} = require("./chui_label");
+const {Select} = require("./inputs/chui_select_box");
 
 const Commands = {
     copy: "copy",
@@ -9,6 +11,8 @@ const Commands = {
     italic: "italic",
     strikeThrough: "strikeThrough",
     underline: "underline",
+    //
+    removeFormat: "removeFormat",
     //
     superscript: "superscript",
     subscript: "subscript",
@@ -24,7 +28,10 @@ class TextEditor {
     #editor_controls = document.createElement('chui_editor_controls');
     #text_input = document.createElement('chui_editor_text_input');
     #status_row = document.createElement('chui_editor_status_row');
-    constructor(height) {
+    // status
+    #cater_position = new Label("0");
+    //
+    constructor(height = Number(undefined)) {
         require('../modules/chui_functions').style_parse([
             {
                 name: "chui_text_editor",
@@ -33,7 +40,11 @@ class TextEditor {
                     "margin": "var(--margin)",
                     "box-shadow": "var(--shadow_one) 0px 0px 2px 0px",
                     "display": "flex",
-                    "flex-direction": "column"
+                    "flex-direction": "column",
+                    "background": "var(--input_background)",
+                    "border": "2px solid var(--input_background)",
+                    //"width": "max-content",
+                    "height": `${height}px`
                 }
             },
             {
@@ -49,10 +60,11 @@ class TextEditor {
                 style: {
                     "width": "-webkit-fill-available",
                     "font-size": "17px",
-                    "padding": "0px 10px 10px 10px",
+                    "padding": "0px 19px 19px 19px",
                     "height": "calc(100% - 40px)",
                     "overflow": "overlay",
-                    "text-align": "start"
+                    "text-align": "start",
+                    "color": "var(--text_color)",
                 }
             },
             {
@@ -70,7 +82,8 @@ class TextEditor {
                     "padding": "9px",
                     "font-size": "12pt",
                     "font-weight": "500",
-                    "background": "transparent",
+                    "margin": "var(--margin)",
+                    "background": "var(--button_background)",
                     "color": "var(--button_text_color)",
                     "box-sizing": "border-box",
                 }
@@ -92,22 +105,14 @@ class TextEditor {
                 }
             }
         ], 'TextEditor');
-        this.#chui_text_editor.style.width = "max-content";
-        this.#chui_text_editor.style.height = height;
-        this.#text_input.addEventListener("keyup", () => {
-            document.execCommand("removeFormat", false)
-            document.execCommand('formatBlock', false, 'div');
-            console.log(this.#getCaretPosition())
-        })
-        this.#text_input.addEventListener("mouseup", () => {
-            console.log(this.#getCaretPosition())
-        })
 
         // FORMAT
         let button_bold_text = new ControlButton(Icons.EDITOR.FORMAT_BOLD, Commands.bold)
         let button_italic_text = new ControlButton(Icons.EDITOR.FORMAT_ITALIC, Commands.italic)
         let button_strikeThrough = new ControlButton(Icons.EDITOR.FORMAT_STRIKETHROUGH, Commands.strikeThrough)
         let button_underline = new ControlButton(Icons.EDITOR.FORMAT_UNDERLINED, Commands.underline)
+        //
+        let button_removeFormat = new ControlButton(Icons.EDITOR.FORMAT_CLEAR, Commands.removeFormat)
         //
         let button_superscript = new ControlButton(Icons.EDITOR.SUPERSCRIPT, Commands.superscript)
         let button_subscript = new ControlButton(Icons.EDITOR.SUBSCRIPT, Commands.subscript)
@@ -117,10 +122,14 @@ class TextEditor {
         let button_justifyRight = new ControlButton(Icons.EDITOR.FORMAT_ALIGN_RIGHT, Commands.justifyRight)
         let button_justifyFull = new ControlButton(Icons.EDITOR.FORMAT_ALIGN_JUSTIFY, Commands.justifyFull)
         //
+        this.#editor_controls.appendChild(new SelectFontSize().set())
+        //
         this.#editor_controls.appendChild(button_bold_text.set());
         this.#editor_controls.appendChild(button_italic_text.set());
         this.#editor_controls.appendChild(button_strikeThrough.set());
         this.#editor_controls.appendChild(button_underline.set());
+        //
+        this.#editor_controls.appendChild(button_removeFormat.set());
         //
         this.#editor_controls.appendChild(button_superscript.set());
         this.#editor_controls.appendChild(button_subscript.set());
@@ -134,6 +143,24 @@ class TextEditor {
         this.#chui_text_editor.appendChild(this.#editor_controls);
         this.#chui_text_editor.appendChild(this.#text_input);
         this.#chui_text_editor.appendChild(this.#status_row);
+        //
+        this.#status_row.appendChild(this.#cater_position.set())
+
+        //
+        this.#text_input.addEventListener("keyup", () => {
+            document.execCommand('formatBlock', false, 'div');
+            this.#cater_position.setText(this.#getCaretPosition().toString());
+        })
+        this.#text_input.addEventListener("mouseup", () => {
+            this.#cater_position.setText(this.#getCaretPosition().toString());
+        })
+
+        this.#text_input.addEventListener('focus', () => {
+            this.#chui_text_editor.style.border = '2px solid var(--blue_prime_background)';
+        })
+        this.#text_input.addEventListener('blur', () => {
+            this.#chui_text_editor.removeAttribute("style");
+        })
     }
     set() {
         return this.#chui_text_editor;
@@ -160,6 +187,19 @@ class TextEditor {
             charCount += range.startOffset;
         }
         return charCount;
+    }
+}
+
+class SelectFontSize {
+    #select = new Select({
+        placeholder:'Размер',
+        width:'20px'
+    });
+    constructor() {
+        this.#select.addOptions("1", "2", "3")
+    }
+    set() {
+        return this.#select.set()
     }
 }
 
