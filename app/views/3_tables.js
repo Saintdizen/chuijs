77bb897ad6c3ffd87dc57110f127068ff002599b
chuvakhin <chuvakhin@hopper-it.ru>
2022-09-07 22:58:07 +0300

    СОРТИРОВКА ТАБЛИЦЫ ВЕРСИЯ АДЫН

--- app/views/3_tables.js
+++ app/views/3_tables.js
@@ -8,42 +8,33 @@ class TablesPage extends Page {
 
         let table  = new Table({
             data: [
-                new Test('Paris specialites', 'France'),
-                new Test('Alfreds Futterkiste', 'Germany'),
-                new Test('Berglunds snabbkop', 'Sweden'),
-                new Test('Island Trading', 'UK'),
-                new Test('Koniglich Essen', 'Germany'),
-                new Test('Magazzini Alimentari Riuniti', 'Italy'),
-                new Test('North/South', 'UK'),
-                new Test('Laughing Bacchus Winecellars', 'Canada'),
+                new Test("ЯД", 3, true),
+                new Test("АНАНАС", 5, false),
+                new Test("ВОДА", 1, true),
             ],
             userSelect: true,
-            customName: ["Столбец 1", "Столбец 2"],
-            columnsWidth: ["50%", "50%"]
+            customName: ["Наименование", "Цена", "Наличие на складе"],
+            columnsWidth: ["50%", "25%", "25%"]
         })
 
         let filter_by_text = new TextInput({ title: "ФИЛЬТРЬ" })
 
         filter_by_text.addInputListener((e) => {
-            console.log(e.target.value.toString())
-            table.setFilterByProperty(Table.FILTER_TYPE.PARTIAL_MATCH, "name1", e.target.value.toString())
+            table.setFilterByProperty(Table.FILTER_TYPE.PARTIAL_MATCH, "name", e.target.value.toString())
             if (e.target.value === "") {
-                table.removeFilterByProperty("name1", e.target.value.toString())
+                table.removeFilterByProperty("name", e.target.value.toString())
             }
         })
 
-        let sort_button = new Button("Сортировать таблицу", () => {
-            table.sortTable()
-        })
-
-        this.add(filter_by_text, table, sort_button)
+        this.add(filter_by_text, table);
     }
 }
 
 class Test {
-    constructor(name1, name2) {
-        this.name1 = name1
-        this.name2 = name2
+    constructor(name, price, status) {
+        this.name = name
+        this.price = price
+        this.status = status
     }
 }
 
--- framework/components/chui_table.js
+++ framework/components/chui_table.js
@@ -1,5 +1,6 @@
 class Table {
     #filtered_data = [];
+    #sorted_data = [];
     #table = document.createElement('table');
     //#caption = this.#table.createCaption();
     #tHead = this.#table.createTHead();
@@ -13,6 +14,7 @@ class Table {
         data: undefined,
         customName: undefined,
         userSelect: Boolean(undefined),
+        sorted: false,
         columnsWidth: []
     }) {
         this.#data = options.data;
@@ -101,10 +103,10 @@ class Table {
     setFilterByProperty(type = String(undefined), property = String(undefined), filterValue = new Object(undefined)) {
         this.#filtered_data = [];
         if (type.includes(Table.FILTER_TYPE.CLEAR_MATCH)) {
-            this.#data.filter(data_1 => data_1[property] === filterValue).forEach(data_2 => { this.#filtered_data.push(data_2); })
+            this.#data.filter(data_1 => data_1[property].toLowerCase() === filterValue.toLowerCase()).forEach(data_2 => { this.#filtered_data.push(data_2); })
         }
         if (type.includes(Table.FILTER_TYPE.PARTIAL_MATCH)) {
-            this.#data.filter(data_1 => data_1[property].includes(filterValue)).forEach(data_2 => { this.#filtered_data.push(data_2); })
+            this.#data.filter(data_1 => data_1[property].toLowerCase().includes(filterValue.toLowerCase())).forEach(data_2 => { this.#filtered_data.push(data_2); })
         }
         this.#setTable(this.#filtered_data)
         if (this.#userSelect !== undefined) {
@@ -112,7 +114,7 @@ class Table {
         }
     }
     removeFilterByProperty(property= String(undefined), filterValue = new Object(undefined)) {
-        this.#data.filter(data_1 => data_1[property] === filterValue).forEach(data_2 => {
+        this.#data.filter(data_1 => data_1[property].toLowerCase() === filterValue.toLowerCase()).forEach(data_2 => {
             this.#filtered_data.splice(this.#filtered_data.indexOf(data_2), 1)
         })
         if (this.#filtered_data.length !== 0) {
@@ -132,6 +134,23 @@ class Table {
         //row.style.borderBottom = '2px solid var(--input_background)'
         for (let head of cols) {
             const cell = row.insertCell()
+            let flag = 1;
+            cell.addEventListener("click", () => {
+                if (flag === 1) {
+                    this.#sortTable(Table.SORT_METHOD.ASC, cell.cellIndex)
+                    flag = 2;
+                } else if (flag === 2) {
+                    this.#sortTable(Table.SORT_METHOD.DESC, cell.cellIndex)
+                    flag = 3;
+                } else if (flag === 3) {
+                    if (this.#filtered_data.length !== 0) {
+                        this.#setTable(this.#filtered_data);
+                    } else {
+                        this.#setTable(this.#data);
+                    }
+                    flag = 1;
+                }
+            })
             cell.style.color = 'var(--button_text_color)'
             if (cell.cellIndex !== 0) {
                 cell.style.borderLeft = '2px solid transparent'
@@ -162,23 +181,58 @@ class Table {
             }
         }
     }
-    sortTable() {
-        this.#sort(this.#data)
+    #sortTable(sortMethod = String(undefined), col = Number(undefined)) {
+        if (this.#filtered_data.length !== 0) {
+            this.#sorted_data = this.#filtered_data;
+            this.#sort(col, sortMethod);
+        } else {
+            this.#sorted_data = this.#data;
+            this.#sort(col, sortMethod);
+        }
     }
-    #sort(dataArray = []) {
-        let cols = Object.getOwnPropertyNames(dataArray);
+    #sort(colIndex = Number(undefined), sortMethod = String(undefined)) {
+        let cols = Object.getOwnPropertyNames(this.#sorted_data[0]);
         let arr = [];
-        for (let data of dataArray) {
+        for (let data of this.#sorted_data) {
             let sub_arr = [];
             for (let i = 0; i < cols.length; i++) sub_arr.push(data[cols[i]]);
             arr.push(sub_arr)
         }
         let sort_data = [];
-        arr.sort().forEach(data => {
-            sort_data.push(new this.#data[0].__proto__.constructor(...data))
-        })
+
+        if (sortMethod.includes(Table.SORT_METHOD.ASC)) {
+            arr.sort((a,b) => {
+                if (typeof a[colIndex] === "string") {
+                    return a[colIndex].localeCompare(b[colIndex])
+                } else if (typeof a[colIndex] === "number") {
+                    if (a[colIndex] === b[colIndex]) {
+                        return 0
+                    } else {
+                        return (a[colIndex] < b[colIndex]) ? -1 : 1;
+                    }
+                }
+            }).forEach(data => {
+                sort_data.push(new this.#data[0].__proto__.constructor(...data))
+            })
+        } else if (sortMethod.includes(Table.SORT_METHOD.DESC)) {
+            arr.sort((a,b) => {
+                if (typeof a[colIndex] === "string") {
+                    return b[colIndex].localeCompare(a[colIndex])
+                } else if (typeof a[colIndex] === "number") {
+                    if (b[colIndex] === a[colIndex]) {
+                        return 0
+                    } else {
+                        return (b[colIndex] < a[colIndex]) ? -1 : 1;
+                    }
+                }
+            }).forEach(data => {
+                sort_data.push(new this.#data[0].__proto__.constructor(...data))
+            })
+        }
+
         this.#setTable(sort_data)
     }
+    static SORT_METHOD = { ASC: "ASC", DESC: "DESC" }
     static FILTER_TYPE = { CLEAR_MATCH: "CLEAR_MATCH", PARTIAL_MATCH: "PARTIAL_MATCH" }
 }
 
