b28ce8010d8f55fa786d31939d1f5c455e5781f9
chuvakhin <chuvakhin@hopper-it.ru>
2022-09-30 14:14:09 +0300

    0.7.0 v1

--- app/views/1_inputs_buttons.js
+++ app/views/1_inputs_buttons.js
@@ -10,93 +10,52 @@ class Inputs_Buttons_Page extends Page {
         this.setTitle('Поля ввода и Кнопки');
         this.setMain(true)
 
-        let h1_inputs = new H(1, "Поля ввода")
-        let inputs_off = new Button("Выключить", () => {
-            text.setDisabled(true);
-            email.setDisabled(true);
-            pass.setDisabled(true);
-            number.setDisabled(true);
-            textArea.setDisabled(true);
-            combo.setDisabled(true);
-            select.setDisabled(true);
-            date.setDisabled(true);
-            file.setDisabled(true);
-            file_multiple.setDisabled(true);
-            checkBox.setDisabled(true);
-            radio.setDisabled(true);
+        // Поля ввода
+        let h1_inputs = new H(1, "Поля ввода");
+        let disabler_1 = new CheckBox({ title: "Выключить поля ввода" })
+        disabler_1.addChangeListener((e) => {
+            button.setDisabled(e.target.checked);
+            text.setDisabled(e.target.checked);
+            email.setDisabled(e.target.checked);
+            pass.setDisabled(e.target.checked);
+            number.setDisabled(e.target.checked);
+            textArea.setDisabled(e.target.checked);
+            combo.setDisabled(e.target.checked);
+            select.setDisabled(e.target.checked);
+            date.setDisabled(e.target.checked);
+            file.setDisabled(e.target.checked);
+            file_multiple.setDisabled(e.target.checked);
+            checkBox.setDisabled(e.target.checked);
+            radio.setDisabled(e.target.checked);
         });
-        let inputs_on = new Button("Включить", () => {
-            text.setDisabled(false);
-            email.setDisabled(false);
-            pass.setDisabled(false);
-            number.setDisabled(false);
-            textArea.setDisabled(false);
-            combo.setDisabled(false);
-            select.setDisabled(false);
-            date.setDisabled(false);
-            file.setDisabled(false);
-            file_multiple.setDisabled(false);
-            checkBox.setDisabled(false);
-            radio.setDisabled(false);
-        });
-        //
         let text = new TextInput({title: "TextInput"})
         text.setValue("TextInput");
-        text.setDisabled(true);
-        //
         let email = new EmailInput({title: "EmailInput"})
         email.setValue("email@input.ru");
-        email.setDisabled(true);
-        //
         let pass = new PasswordInput({title: "PasswordInput"})
         pass.setValue("1234567890");
-        pass.setDisabled(true);
-        //
         let number = new NumberInput({title: "NumberInput"})
         number.setValue("1000");
-        number.setDisabled(true);
-        //
         let textArea = new TextArea({title: "TextArea"})
         textArea.setValue("1000");
-        textArea.setDisabled(true);
-        //
         let combo = new ComboBox({title: 'ComboBox'})
         combo.addOptions("Option 1", "Option 2", "Option 3")
-        combo.setDisabled(true);
-        //
         let select = new Select({title: 'Select'})
         select.addOptions("Option 1", "Option 2", "Option 3")
         select.setDefaultOption("Option 1")
-        select.setDisabled(true);
-        //
         let date = new DateInput({title: "DateInput"})
-        date.setDisabled(true);
-        //
         let file = new FileInput({ title: "FileInput", multiple: false })
-        file.setDisabled(true);
-        //
         let file_multiple = new FileInput({ title: "FileInput_multiple", multiple: true })
-        file_multiple.setDisabled(true);
-        //
         let checkBox = new CheckBox({ title: "CheckBox" })
-        checkBox.setDisabled(true);
-        //
         let radio = new RadioButton({ title: "RadioButton" })
-        radio.setDisabled(true);
-        //
-        this.add(h1_inputs, inputs_off, inputs_on, text, email, pass, number, textArea, combo, select, date, file, file_multiple, checkBox, radio)
+        this.add(h1_inputs, disabler_1, text, email, pass, number, textArea, combo, select, date, file, file_multiple, checkBox, radio)
 
         // Кнопки
         let h1_buttons = new H(1, "Кнопки");
-        let buttons_off = new Button("Выключить", () => {
-            button.setDisabled(true)
-        });
-        let buttons_on = new Button("Включить", () => {
-            button.setDisabled(false)
-        });
+        let disabler_2 = new CheckBox({ title: "Выключить кнопки" })
+        disabler_2.addChangeListener((e) => button.setDisabled(e.target.checked))
         let button = new Button("Обычная кнопка", (e) => console.log(e));
-        button.setDisabled(true)
-        this.add(h1_buttons, buttons_off, buttons_on, button);
+        this.add(h1_buttons, disabler_2, button);
     }
 }
 
