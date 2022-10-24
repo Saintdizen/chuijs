ae168bad9b26f31647825ac567aef2f239c9c478
chuvakhin <chuvakhin@hopper-it.ru>
2022-10-24 17:35:52 +0300

    1.0.3

--- framework/components/chui_image.js
+++ framework/components/chui_image.js
@@ -1,9 +1,11 @@
 const {Animation} = require("../modules/chui_animations");
 const sizeOf = require("image-size");
 const electron = require("electron");
+const {Icon, Icons} = require("./chui_icons");
 
 class Image {
     #chui_image = document.createElement('chui_image');
+    #image = document.createElement('img');
     constructor(options = {
         path: String(),
         base64: String(),
@@ -48,31 +50,64 @@ class Image {
                 }
             },
             {
-                name: "image_popup_content",
+                name: "popup_content_test",
                 style: {
-                    "margin": "auto",
                     "width": "-webkit-fill-available",
                     "height": "-webkit-fill-available",
                     "display": "flex",
                     "align-items": "center",
                     "justify-content": "center",
                 }
+            },
+            {
+                name: "image_popup_content",
+                style: {
+                    "width": "max-content",
+                    "height": "max-content",
+                    "display": "block",
+                }
+            },
+            {
+                name: "image_popup_close",
+                style: {
+                    "color": "var(--text_color)",
+                    "position": "absolute",
+                    "top": "48px",
+                    "right": "48px",
+                    "width": "60px",
+                    "height": "60px",
+                    "display": "flex",
+                    "justify-content": "center",
+                    "align-items": "center",
+                    "border-radius": "var(--border_radius)",
+                }
+            },
+            {
+                name: "image_popup_close:hover",
+                style: {
+                    "background-color": "var(--button_background)",
+                }
+            },
+            {
+                name: "image_popup_close:hover chui_icon",
+                style: {
+                    "color": "var(--button_text_color)",
+                }
             }
         ], 'chUiJS_Image');
-        let image = document.createElement('img');
-        image.style.width = options.width //'-webkit-fill-available';
-        image.style.height = options.height // '-webkit-fill-available';
         if (options.disableMargin) this.#chui_image.style.margin = '0px';
-        if (options.path !== undefined) image.setAttribute('src', `data:image/png;base64,${require('fs').readFileSync(options.path).toString("base64")}`)
-        if (options.base64 !== undefined) image.setAttribute('src', `data:image/png;base64,${options.base64}`);
+        if (options.path !== undefined) this.#image.setAttribute('src', `data:image/png;base64,${require('fs').readFileSync(options.path).toString("base64")}`)
+        if (options.base64 !== undefined) this.#image.setAttribute('src', `data:image/png;base64,${options.base64}`);
         if (options.width !== undefined) this.#chui_image.style.width = options.width;
         if (options.height !== undefined) this.#chui_image.style.height = options.height;
-        this.#chui_image.appendChild(image);
+        if (options.width !== undefined) this.#image.style.width = options.width;
+        if (options.height !== undefined) this.#image.style.height = options.height;
+        this.#chui_image.appendChild(this.#image);
 
         if (options.openPopup) {
             this.#chui_image.addEventListener("click", () => {
                 let popup;
-                const widow_size = document.body.getBoundingClientRect();
+                let widow_size = document.body.getBoundingClientRect();
                 if (options.path !== undefined) {
                     //
                     let imageSet;
@@ -86,22 +121,19 @@ class Image {
                     }
                 }
                 if (options.base64 !== undefined) {
-                    let image_size; let popup_size; let imageSet; let img = Buffer.from(options.base64, 'base64'); let dimensions = sizeOf(img);
-
-                    if (widow_size.width < dimensions.width) {
-                        image_size = ["-webkit-fill-available", "auto"];
-                    } else if (widow_size.height < dimensions.height) {
-                        image_size = ["auto", "-webkit-fill-available"];
-                    } else if (widow_size.width > dimensions.width) {
-                        image_size = [`auto`, ""];
+                    let image_size; let imageSet; let img = Buffer.from(options.base64, 'base64'); let dimensions = sizeOf(img);
+                    if (dimensions.height > widow_size.height) {
+                        image_size = ["auto", `${widow_size.height}px`]
+                    } else if (dimensions.width > widow_size.width) {
+                        image_size = [`${widow_size.height}px`, "auto"]
                     } else {
-                        image_size = [`-webkit-fill-available`, "-webkit-fill-available"];
+                        image_size = ["auto", `auto`]
                     }
-                    popup_size = ["93%", "93%"];
-
                     //
                     imageSet = new Image({ disableMargin: true, openPopup: false, base64: options.base64, width: image_size[0], height: image_size[1] });
-                    popup = new ImagePopup(imageSet.set(), popup_size[0], popup_size[1]);
+                    popup = new ImagePopup(imageSet.set(), "max-content", "max-content");
+
+
                 }
                 document.getElementById("app").appendChild(popup.set())
                 popup.open();
@@ -110,9 +142,11 @@ class Image {
     }
     setWidth(value) {
         this.#chui_image.style.width = value;
+        this.#chui_image.style.width = value;
     }
     setHeight(value) {
         this.#chui_image.style.height = value;
+        this.#chui_image.style.width = value;
     }
     set() { return this.#chui_image; }
 }
@@ -120,18 +154,22 @@ class Image {
 class ImagePopup {
     #id = require("randomstring").generate();
     #chui_image_popup = document.createElement(`chui_image_popup`);
+    #popup_content = document.createElement('popup_content_test');
     #image_popup_content = document.createElement('image_popup_content');
+    #close_popup = document.createElement("image_popup_close")
     constructor(image, width, height) {
         this.#chui_image_popup.id = this.#id
-        window.addEventListener('click', (event) => {
-            let elem = document.getElementById(this.#id);
-            if (event.target === elem) new Animation(elem).fadeOut();
-        })
         //ADDS
         this.#image_popup_content.style.width = width
         this.#image_popup_content.style.height = height
         this.#image_popup_content.appendChild(image);
-        this.#chui_image_popup.appendChild(this.#image_popup_content);
+
+        this.#close_popup.innerHTML = new Icon(Icons.NAVIGATION.CLOSE, "30px", "").getHTML();
+        this.#close_popup.addEventListener("click", () => this.close());
+
+        this.#popup_content.appendChild(this.#image_popup_content)
+        this.#popup_content.appendChild(this.#close_popup);
+        this.#chui_image_popup.appendChild(this.#popup_content);
     }
     open() {
         let dialog = document.getElementById(this.#id);
@@ -141,7 +179,7 @@ class ImagePopup {
     close() {
         let dialog = document.getElementById(this.#id);
         new Animation(dialog.firstChild).scaleOut();
-        new Animation(dialog).fadeOut();
+        new Animation(dialog).fadeOutAndRemove();
     }
     set() {
         return this.#chui_image_popup;
